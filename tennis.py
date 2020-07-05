d594ea6b5e09bec73b750f8e20770bcc33a7bda7
Josh Crompton <josh.crompton@gmail.com>
2020-07-05 15:37:33 +1200

    Pass player names at class initialisation
    Somehow I missed that the player names are provided to the `Match`
    class when it's initialised.

--- tennis.py
+++ tennis.py
@@ -63,10 +63,12 @@ class Game:
     MINIMUM_POINTS = 4
     MINIMUM_LEAD = 2
 
-    def __init__(self):
+    def __init__(self, player_one, player_two):
+        self.player_one = player_one
+        self.player_two = player_two
         self._points = {
-            PLAYER_ONE: 0,
-            PLAYER_TWO: 0,
+            self.player_one: 0,
+            self.player_two: 0,
         }
         self._scores = {
             0: 0,
@@ -87,27 +89,27 @@ class Game:
         if self.winner():
             return ""
 
-        if self._points[PLAYER_ONE] >= 3 and self._points[PLAYER_TWO] >=3:
-            if self._points[PLAYER_ONE] > self._points[PLAYER_TWO]:
-                return f"Advantage {PLAYER_ONE}"
-            elif self._points[PLAYER_ONE] < self._points[PLAYER_TWO]:
-                return f"Advantage {PLAYER_TWO}"
+        if self._points[self.player_one] >= 3 and self._points[self.player_two] >=3:
+            if self._points[self.player_one] > self._points[self.player_two]:
+                return f"Advantage {self.player_one}"
+            elif self._points[self.player_one] < self._points[self.player_two]:
+                return f"Advantage {self.player_two}"
             else:
                 return "Deuce"
 
         return "-".join([
-            self._score(PLAYER_ONE),
-            self._score(PLAYER_TWO),
+            self._score(self.player_one),
+            self._score(self.player_two),
         ])
 
     def winner(self):
         """Return the winner of the game."""
-        if (self._points[PLAYER_ONE] >= self.MINIMUM_POINTS and
-            self._points[PLAYER_ONE] - self._points[PLAYER_TWO] >= self.MINIMUM_LEAD):
-            return PLAYER_ONE
-        if (self._points[PLAYER_TWO] >= self.MINIMUM_POINTS and
-            self._points[PLAYER_TWO] - self._points[PLAYER_ONE] >= self.MINIMUM_LEAD):
-            return PLAYER_TWO
+        if (self._points[self.player_one] >= self.MINIMUM_POINTS and
+            self._points[self.player_one] - self._points[self.player_two] >= self.MINIMUM_LEAD):
+            return self.player_one
+        if (self._points[self.player_two] >= self.MINIMUM_POINTS and
+            self._points[self.player_two] - self._points[self.player_one] >= self.MINIMUM_LEAD):
+            return self.player_two
 
 
 class TieBreakGame(Game):
@@ -122,30 +124,32 @@ class TieBreakGame(Game):
 class Set:
     """A single Set."""
 
-    def __init__(self):
-        self._games = [Game()]
+    def __init__(self, player_one, player_two):
+        self.player_one = player_one
+        self.player_two = player_two
+        self._games = [Game(self.player_one, self.player_two)]
 
     def point_won_by(self, player):
         if self._games[-1].winner():
-            if self.games_won()[PLAYER_ONE] == 6 and self.games_won()[PLAYER_TWO] == 6:
-                game = TieBreakGame()
+            if self.games_won()[self.player_one] == 6 and self.games_won()[self.player_two] == 6:
+                game = TieBreakGame(self.player_one, self.player_two)
             else:
-                game = Game()
+                game = Game(self.player_one, self.player_two)
             self._games.append(game)
         self._games[-1].point_won_by(player)
 
     def games_won(self):
         winners = [game.winner() for game in self._games]
         return {
-            PLAYER_ONE: winners.count(PLAYER_ONE),
-            PLAYER_TWO: winners.count(PLAYER_TWO),
+            self.player_one: winners.count(self.player_one),
+            self.player_two: winners.count(self.player_two),
         }
 
     def score(self):
         """Return the current score."""
         return "-".join([
-            str(self.games_won()[PLAYER_ONE]),
-            str(self.games_won()[PLAYER_TWO])
+            str(self.games_won()[self.player_one]),
+            str(self.games_won()[self.player_two])
         ])
 
     def winner(self):
@@ -153,16 +157,19 @@ class Set:
         if isinstance(self._games[-1], TieBreakGame):
             return self._games[-1].winner()
 
-        games_player_one = self.games_won()[PLAYER_ONE]
-        games_player_two = self.games_won()[PLAYER_TWO]
+        games_player_one = self.games_won()[self.player_one]
+        games_player_two = self.games_won()[self.player_two]
         if games_player_one > 6 and games_player_one > games_player_two + 2:
-            return PLAYER_ONE
+            return self.player_one
         if games_player_two > 6 and games_player_two > games_player_one + 2:
-            return PLAYER_TWO
+            return self.player_two
 
 
 class Match:
     """A Tennis Match."""
+
+    def __init__(self, player_one, player_two):
+        pass
 
     def score(self):
         """Return the current set score followed by the current game score."""
--- test_tennis.py
+++ test_tennis.py
@@ -11,141 +11,141 @@ from tennis import Match, Game, Set, TieBreakGame
 class MatchTestCase(unittest.TestCase):
 
     def test_score_with_no_points(self):
-        match = Match()
+        match = Match("player 1", "player 2")
         self.assertEqual(match.score(), "0-0")
 
 
 class GameTestCase(unittest.TestCase):
 
     def test_score_with_no_points_is_empty(self):
-        game = Game()
+        game = Game("player 1", "player 2")
         self.assertEqual(game.score(), "0-0")
 
     def test_score_with_one_point(self):
-        game = Game()
-        game.point_won_by("player-1")
+        game = Game("player 1", "player 2")
+        game.point_won_by("player 1")
 
         self.assertEqual(game.score(), "15-0")
 
     def test_score_with_two_points(self):
-        game = Game()
-        game.point_won_by("player-1")
-        game.point_won_by("player-1")
+        game = Game("player 1", "player 2")
+        game.point_won_by("player 1")
+        game.point_won_by("player 1")
 
         self.assertEqual(game.score(), "30-0")
 
     def test_score_with_three_points(self):
-        game = Game()
-        game.point_won_by("player-1")
-        game.point_won_by("player-1")
-        game.point_won_by("player-1")
+        game = Game("player 1", "player 2")
+        game.point_won_by("player 1")
+        game.point_won_by("player 1")
+        game.point_won_by("player 1")
 
         self.assertEqual(game.score(), "40-0")
 
     def test_score_with_mixed_points(self):
-        game = Game()
-        game.point_won_by("player-1")
-        game.point_won_by("player-2")
-        game.point_won_by("player-1")
+        game = Game("player 1", "player 2")
+        game.point_won_by("player 1")
+        game.point_won_by("player 2")
+        game.point_won_by("player 1")
 
         self.assertEqual(game.score(), "30-15")
 
     def test_deuce_for_four_points(self):
-        game = Game()
+        game = Game("player 1", "player 2")
         for _ in range(4):
-            game.point_won_by("player-1")
-            game.point_won_by("player-2")
+            game.point_won_by("player 1")
+            game.point_won_by("player 2")
 
         self.assertEqual(game.score(), "Deuce")
 
     def test_deuce_for_five_points(self):
-        game = Game()
+        game = Game("player 1", "player 2")
         for _ in range(5):
-            game.point_won_by("player-1")
-            game.point_won_by("player-2")
+            game.point_won_by("player 1")
+            game.point_won_by("player 2")
 
         self.assertEqual(game.score(), "Deuce")
 
     def test_advantage_player_one_for_four_points(self):
-        game = Game()
+        game = Game("player 1", "player 2")
         for _ in range(4):
-            game.point_won_by("player-1")
+            game.point_won_by("player 1")
         for _ in range(3):
-            game.point_won_by("player-2")
+            game.point_won_by("player 2")
 
-        self.assertEqual(game.score(), "Advantage player-1")
+        self.assertEqual(game.score(), "Advantage player 1")
 
     def test_advantage_player_two_for_four_points(self):
-        game = Game()
+        game = Game("player 1", "player 2")
         for _ in range(3):
-            game.point_won_by("player-1")
+            game.point_won_by("player 1")
         for _ in range(4):
-            game.point_won_by("player-2")
+            game.point_won_by("player 2")
 
-        self.assertEqual(game.score(), "Advantage player-2")
+        self.assertEqual(game.score(), "Advantage player 2")
 
     def test_player_one_wins_all(self):
-        game = Game()
+        game = Game("player 1", "player 2")
         for _ in range(5):
-            game.point_won_by("player-1")
+            game.point_won_by("player 1")
 
-        self.assertEqual(game.winner(), "player-1")
+        self.assertEqual(game.winner(), "player 1")
 
     def test_player_two_wins_all(self):
-        game = Game()
+        game = Game("player 1", "player 2")
         for _ in range(5):
-            game.point_won_by("player-2")
+            game.point_won_by("player 2")
 
-        self.assertEqual(game.winner(), "player-2")
+        self.assertEqual(game.winner(), "player 2")
 
     def test_player_one_ahead_by_one_no_winner(self):
-        game = Game()
+        game = Game("player 1", "player 2")
         for _ in range(5):
-            game.point_won_by("player-1")
+            game.point_won_by("player 1")
 
         for _ in range(4):
-            game.point_won_by("player-2")
+            game.point_won_by("player 2")
 
         self.assertIsNone(game.winner())
 
     def test_player_two_ahead_by_one_no_winner(self):
-        game = Game()
+        game = Game("player 1", "player 2")
         for _ in range(4):
-            game.point_won_by("player-1")
+            game.point_won_by("player 1")
 
         for _ in range(5):
-            game.point_won_by("player-2")
+            game.point_won_by("player 2")
 
         self.assertIsNone(game.winner())
 
     def test_player_one_wins_closest_possible_match(self):
-        game = Game()
+        game = Game("player 1", "player 2")
         for _ in range(2):
-            game.point_won_by("player-2")
+            game.point_won_by("player 2")
 
         for _ in range(4):
-            game.point_won_by("player-1")
+            game.point_won_by("player 1")
 
-        self.assertEqual(game.winner(), "player-1")
+        self.assertEqual(game.winner(), "player 1")
 
     def test_player_two_wins_closest_possible_match(self):
-        game = Game()
+        game = Game("player 1", "player 2")
         for _ in range(2):
-            game.point_won_by("player-1")
+            game.point_won_by("player 1")
 
         for _ in range(4):
-            game.point_won_by("player-2")
+            game.point_won_by("player 2")
 
-        self.assertEqual(game.winner(), "player-2")
+        self.assertEqual(game.winner(), "player 2")
 
 
     def test_score_is_empty_when_game_is_won(self):
-        game = Game()
+        game = Game("player 1", "player 2")
         for _ in range(4):
-            game.point_won_by("player-1")
+            game.point_won_by("player 1")
 
         for _ in range(6):
-            game.point_won_by("player-2")
+            game.point_won_by("player 2")
 
         self.assertEqual(game.score(), "")
 
@@ -157,96 +157,96 @@ class SetTestCase(unittest.TestCase):
     POINTS_TO_WIN_TIE_BREAKER = 7
 
     def test_initial_set_score(self):
-        set_ = Set()
+        set_ = Set("player 1", "player 2")
 
         self.assertEqual(set_.score(), "0-0")
 
     def test_score_when_no_games_won(self):
-        set_ = Set()
-        set_.point_won_by("player-1")
+        set_ = Set("player 1", "player 2")
+        set_.point_won_by("player 1")
 
         self.assertEqual(set_.score(), "0-0")
 
     def test_score_when_player_one_wins_a_game(self):
-        set_ = Set()
+        set_ = Set("player 1", "player 2")
         for _ in range(self.POINTS_TO_WIN_GAME):
-            set_.point_won_by("player-1")
+            set_.point_won_by("player 1")
 
         self.assertEqual(set_.score(), "1-0")
 
     def test_score_when_player_two_wins_a_game(self):
-        set_ = Set()
+        set_ = Set("player 1", "player 2")
         for _ in range(self.POINTS_TO_WIN_GAME):
-            set_.point_won_by("player-2")
+            set_.point_won_by("player 2")
 
         self.assertEqual(set_.score(), "0-1")
 
     def test_no_initial_winner(self):
-        set_ = Set()
+        set_ = Set("player 1", "player 2")
 
         self.assertIsNone(set_.winner())
 
     def test_no_winner_if_no_games_won(self):
-        set_ = Set()
+        set_ = Set("player 1", "player 2")
         for _ in range(self.POINTS_TO_WIN_GAME - 1):
-            set_.point_won_by("player-1")
+            set_.point_won_by("player 1")
 
         self.assertIsNone(set_.winner())
 
     def test_no_winner_if_one_game_won(self):
-        set_ = Set()
+        set_ = Set("player 1", "player 2")
         for _ in range(self.POINTS_TO_WIN_GAME):
-            set_.point_won_by("player-1")
+            set_.point_won_by("player 1")
 
         self.assertIsNone(set_.winner())
 
     def test_winner_player_one(self):
-        set_ = Set()
+        set_ = Set("player 1", "player 2")
         for _games in range(self.GAMES_TO_WIN_SET):
             for _ in range(self.POINTS_TO_WIN_GAME):
-                set_.point_won_by("player-1")
+                set_.point_won_by("player 1")
 
-        self.assertEqual(set_.winner(), "player-1")
+        self.assertEqual(set_.winner(), "player 1")
 
     def test_winner_player_two(self):
-        set_ = Set()
+        set_ = Set("player 1", "player 2")
         for _games in range(self.GAMES_TO_WIN_SET):
             for _ in range(self.POINTS_TO_WIN_GAME):
-                set_.point_won_by("player-2")
+                set_.point_won_by("player 2")
 
-        self.assertEqual(set_.winner(), "player-2")
+        self.assertEqual(set_.winner(), "player 2")
 
     def test_tie_is_broken_by_player_one(self):
-        set_ = Set()
+        set_ = Set("player 1", "player 2")
         for _games in range(6):
             for _ in range(self.POINTS_TO_WIN_GAME):
-                set_.point_won_by("player-1")
+                set_.point_won_by("player 1")
 
         for _games in range(6):
             for _ in range(self.POINTS_TO_WIN_GAME):
-                set_.point_won_by("player-2")
+                set_.point_won_by("player 2")
 
         # Final game is a tie-breaker
         for _ in range(self.POINTS_TO_WIN_TIE_BREAKER):
-            set_.point_won_by("player-1")
+            set_.point_won_by("player 1")
 
-        self.assertEqual(set_.winner(), "player-1")
+        self.assertEqual(set_.winner(), "player 1")
 
     def test_tie_is_broken_by_player_two(self):
-        set_ = Set()
+        set_ = Set("player 1", "player 2")
         for _games in range(6):
             for _ in range(self.POINTS_TO_WIN_GAME):
-                set_.point_won_by("player-1")
+                set_.point_won_by("player 1")
 
         for _games in range(6):
             for _ in range(self.POINTS_TO_WIN_GAME):
-                set_.point_won_by("player-2")
+                set_.point_won_by("player 2")
 
         # Final game is a tie-breaker
         for _ in range(self.POINTS_TO_WIN_TIE_BREAKER):
-            set_.point_won_by("player-2")
+            set_.point_won_by("player 2")
 
-        self.assertEqual(set_.winner(), "player-2")
+        self.assertEqual(set_.winner(), "player 2")
 
 
 class TieBreakGameTest(unittest.TestCase):
@@ -255,80 +255,80 @@ class TieBreakGameTest(unittest.TestCase):
     MINIMUM_LEAD = 2
 
     def test_initial_winner(self):
-        game = TieBreakGame()
+        game = TieBreakGame("player 1", "player 2")
 
         self.assertIsNone(game.winner())
 
     def test_winner_player_one(self):
-        game = TieBreakGame()
+        game = TieBreakGame("player 1", "player 2")
 
         for _ in range(self.MINIMUM_POINTS + self.MINIMUM_LEAD):
-            game.point_won_by("player-1")
+            game.point_won_by("player 1")
 
-        self.assertEqual(game.winner(), "player-1")
+        self.assertEqual(game.winner(), "player 1")
 
     def test_winner_player_two(self):
-        game = TieBreakGame()
+        game = TieBreakGame("player 1", "player 2")
 
         for _ in range(self.MINIMUM_POINTS + self.MINIMUM_LEAD):
-            game.point_won_by("player-2")
+            game.point_won_by("player 2")
 
-        self.assertEqual(game.winner(), "player-2")
+        self.assertEqual(game.winner(), "player 2")
 
     def test_player_one_wins_closest_possible_tiebreaker(self):
-        game = TieBreakGame()
+        game = TieBreakGame("player 1", "player 2")
         for _ in range(5):
-            game.point_won_by("player-2")
+            game.point_won_by("player 2")
 
         for _ in range(7):
-            game.point_won_by("player-1")
+            game.point_won_by("player 1")
 
-        self.assertEqual(game.winner(), "player-1")
+        self.assertEqual(game.winner(), "player 1")
 
     def test_player_two_wins_closest_possible_tiebreaker(self):
-        game = TieBreakGame()
+        game = TieBreakGame("player 1", "player 2")
         for _ in range(5):
-            game.point_won_by("player-1")
+            game.point_won_by("player 1")
 
         for _ in range(7):
-            game.point_won_by("player-2")
+            game.point_won_by("player 2")
 
-        self.assertEqual(game.winner(), "player-2")
+        self.assertEqual(game.winner(), "player 2")
 
     def test_score_with_no_points(self):
-        game = TieBreakGame()
+        game = TieBreakGame("player 1", "player 2")
 
         self.assertEqual(game.score(), "0-0")
 
     def test_score_with_player_one_points(self):
-        game = TieBreakGame()
-        game.point_won_by("player-1")
-        game.point_won_by("player-1")
-        game.point_won_by("player-1")
+        game = TieBreakGame("player 1", "player 2")
+        game.point_won_by("player 1")
+        game.point_won_by("player 1")
+        game.point_won_by("player 1")
 
         self.assertEqual(game.score(), "3-0")
 
     def test_score_with_player_two_points(self):
-        game = TieBreakGame()
-        game.point_won_by("player-2")
-        game.point_won_by("player-2")
+        game = TieBreakGame("player 1", "player 2")
+        game.point_won_by("player 2")
+        game.point_won_by("player 2")
 
         self.assertEqual(game.score(), "0-2")
 
     def test_score_with_mixed_points(self):
-        game = TieBreakGame()
-        game.point_won_by("player-2")
-        game.point_won_by("player-1")
-        game.point_won_by("player-2")
-        game.point_won_by("player-2")
-        game.point_won_by("player-2")
+        game = TieBreakGame("player 1", "player 2")
+        game.point_won_by("player 2")
+        game.point_won_by("player 1")
+        game.point_won_by("player 2")
+        game.point_won_by("player 2")
+        game.point_won_by("player 2")
 
         self.assertEqual(game.score(), "1-4")
 
     def test_score_is_empty_when_game_is_won(self):
-        game = TieBreakGame()
+        game = TieBreakGame("player 1", "player 2")
         for _ in range(self.MINIMUM_POINTS + self.MINIMUM_LEAD):
-            game.point_won_by("player-2")
+            game.point_won_by("player 2")
 
         self.assertEqual(game.score(), "")
 
